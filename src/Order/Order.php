63232e0356a6e3f52df621a5558cba9fbcc21716
ydg <1028180290@qq.com>
2024-12-09 10:57:12 +0800

    chore: remove useless code

--- src/Order/Order.php
+++ src/Order/Order.php
@@ -8,17 +8,6 @@ use Ydg\JdUnionOpenSdk\JdUnionClient;
 
 class Order extends JdUnionClient
 {
-    /**
-     * 订单行查询接口
-     *
-     * @param array $params
-     *
-     * @return array|null
-     */
-    public function orderRowQuery(array $params): ?array
-    {
-        return $this->rowQuery($params);
-    }
 
     /**
      * 订单行查询接口（https://union.jd.com/openplatform/api/v2?apiName=jd.union.open.order.row.query）
